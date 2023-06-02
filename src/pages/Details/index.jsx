ee334dff9296df5e9bd9fb4737efd1f007a204d4
JoaoLuiss <joaoluisseguro@gmail.com>
2023-06-02 19:01:27 -0300

    Implementando preenchimento da página de Detalhes das Notas

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
@@ -7,32 +11,71 @@ import { ButtonText } from '../../components/ButtonText';
 import { Container, Links, Content } from './styles';
 
 export function Details() {
+  const [data, setData] = useState(null);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate('/')
+  }
+
+  useEffect( () => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+    fetchNote();
+  }, []);
   return(
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title='Excluir nota' />
+      {
+        data && 
+        <main>
+          <Content>
+            <ButtonText title='Excluir nota' />
 
-          <h1>Introdução ao React</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque soluta saepe maiores voluptatem accusamus facere perspiciatis totam vitae. Rerum non temporibus esse modi ducimus debitis veniam, tempora eveniet minima maxime.</p>
+            <h1>{data.title}</h1>
 
-          <Section title='Links úteis'>
-            <Links>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-            </Links>
-          </Section>
+            <p>{data.description}</p>
 
-          <Section title='Marcadores'>
-            <Tag title='express'/>
-            <Tag title='node'/>
-            </Section>
-          
-          <Button title='Voltar' />
-        </Content>
-      </main>
+            {
+              data.links &&
+              <Section title='Links úteis'>
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target='_blank'>
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+             }
+
+            {
+              data.tags &&
+              <Section title='Marcadores'>
+                {
+                  data.tags.map( tag => (
+                      <Tag 
+                        key={String(tag.id)}
+                        title={tag.name}
+                      />
+                    )
+                  )
+                }
+              </Section>
+            }
+            
+            <Button title='Voltar' onClick={handleBack} />
+          </Content>
+        </main>
+      }
     </Container>
   )
 }
