f4c322c78ffc1bef1fe1b5b77d65764447805def
JoaoLuiss <joaoluisseguro@gmail.com>
2023-06-03 12:24:26 -0300

    🐛Corrigindo bug ao fazer SignOut de dentro da página de Detalhe
    O bug era que ao deslogar de dentro da página Details, o usuário não era redirecionado para a tela de login, ficando com a tela vazia.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,20 @@ import { useAuth } from '../../hooks/auth'
 import { Container, Profile, Logout } from "./styles";
 import { api } from '../../services/api';
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
 
   const { signOut, user } = useAuth();
+  const navigate = useNavigate();
 
   const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+  function handleSignOut() {
+    navigate('/');
+    signOut();
+    
+  }
   
   return(
     <Container>
@@ -24,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
