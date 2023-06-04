b1e8a425025358c8a7e84d4e46adbfa6f1cf5b4d
JoaoLuiss <joaoluisseguro@gmail.com>
2023-06-04 16:51:01 -0300

    Alterar endereço destino para requisições à API

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // esse comando "axios.create" cria uma requisição HTTP (pelo q eu entendi)
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://rocketnotes-api-yimi.onrender.com'
 });
